f46693dfaf4616300c10c594bce4cb654bc5f71a
dmitriy30011997 <112504672+dmitriy30011997@users.noreply.github.com>
2023-01-04 02:40:17 +0300

    pushing new file2

---
+++ first_lecture.java
@@ -0,0 +1,7 @@
+// git init
+// git add "name of file"
+// git commit -m "name of commit"
+// git branch -M name_of_branch
+// git remote add origin "link"
+// git push -u origin "name of branch"
+hui
